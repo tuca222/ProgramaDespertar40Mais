12e823de29c77e57f2ccb7ec72155723311dfc60
Arthur Lorenzetti <tucaa222@gmail.com>
2024-10-29 21:32:19 -0300

    logs proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,21 +1,21 @@
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-      try {
-        // Faz a requisição ao Google Apps Script
-        const response = await fetch('https://script.google.com/macros/s/AKfycbxcs4d4B6DHvNPunDDWgoD6SDi5lDmmZgbYY9z8gwRHS-igK0JDkgRpnJjGswzoB6yTUA/exec', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(req.body)
-        });
-  
-        const data = await response.json();
-        res.status(200).json(data);
-      } catch (error) {
-        res.status(500).json({ error: 'Erro ao comunicar com o Google Apps Script' });
-      }
+        try {
+            // Faz a requisição ao Google Apps Script
+            const response = await fetch('https://script.google.com/macros/s/AKfycbxcs4d4B6DHvNPunDDWgoD6SDi5lDmmZgbYY9z8gwRHS-igK0JDkgRpnJjGswzoB6yTUA/exec', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(req.body)
+            });
+
+            const data = await response.json(); // Verifica a resposta do Google Apps Script
+            res.status(200).json(data); // Retorna a resposta para o cliente
+        } catch (error) {
+            console.error("Erro ao comunicar com o Google Apps Script:", error);
+            res.status(500).json({ error: 'Erro ao comunicar com o Google Apps Script' });
+        }
     } else {
-      res.setHeader('Allow', ['POST']);
-      res.status(405).end(`Método ${req.method} não permitido`);
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Método ${req.method} não permitido`);
     }
-  }
-  
+}
